f46d0cae152ce7e1d589dc3d23475abab895de15
sevalenciagr <sevalenciagr@gmail.com>
2024-02-22 12:02:08 -0500

    lazy load

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+  styleUrl: './lazy-image.component.css'
+})
+export class LazyImageComponent implements OnInit {
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt!: string;
+
+  ngOnInit(): void {
+    if ( !this.url )  throw new Error('URL property is required');
+  }
+}
