1f910e611a7aab4168a33e6b2db55ded403d834b
sevalenciagr <sevalenciagr@gmail.com>
2024-02-21 19:13:06 -0500

    Mostrando gifs en cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
@@ -14,6 +16,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     GifsModule,
     SharedModule
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
+
 
 @Component({
   selector: 'shared-sidebar',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+
+  constructor(
+    private  gifsService: GifsService
+    ) { }
+
+    get tags(): string[] {
+      return this.gifsService.tagsHistory;
+    }
+
+    searchTag( tag: string ) {
+
+      this.gifsService.searchTag( tag );
+
+    }
 
 }
